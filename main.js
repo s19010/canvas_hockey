299f4041ea297667363fdae6a3ffee4781acd508
s19010 <s19010@std.it-college.ac.jp>
2020-02-12 16:56:01 +0900

    正式なやつ

--- main.js
+++ main.js
@@ -15,9 +15,6 @@ var BoardDirection = {
 	Right: 1
 };
 
-var counter = 0;
-var count = 0;
-
 function Puck(x, y) {
 	var self = this;
 
@@ -46,11 +43,9 @@ function Puck(x, y) {
 				score1 ++;
 				self.reset(BoardDirection.Right);
 			}
-
-			count = 2;
 		}
 
-		// drawScoreBounce off left wall
+		// Bounce off left wall
 		if (self.x - self.radius < 0) {
 			self.vel.x *= -1;
 			self.x = self.radius;
@@ -65,8 +60,6 @@ function Puck(x, y) {
 		if (self.y + self.radius > boardHeight) {
 			self.vel.y *= -1;
 			self.y = boardHeight - self.radius;
-
-			count = 1;
 		}
 
 		// Bounce off top wall
@@ -76,39 +69,19 @@ function Puck(x, y) {
 		}
 	}
 
-	self.drawPuck = function (context) {
-		/* var fillColor = "white";
+	self.draw = function (context) {
+		var fillColor = "white";
 
 		if (self.closestPointOnPaddle(paddle1) || self.closestPointOnPaddle(paddle2)) {
 			fillColor = "red";
-		} */
-
-		if (count == 0) {
-			context.fillStyle = "white";
-		} else {
-			if (count == 1) {
-			context.fillStyle = "pink";
-			// context.fillStyle = "black"; // 消える魔球
-				console.log(count);
-			} else if (count = 2) {
-			context.fillStyle = "yellow";
-			// context.fillStyle = "black"; // 消える魔球
-				console.log(count);
-			} else if (count == 3) {
-			context.fillStyle = "white";
-				console.log(count);
-			} else if (count == 4) {
-			context.fillStyle = "orange";
-				console.log(count);
-			} else {
-				context.fillStyle= "white";
-			};
 		}
-		// context.fillStyle = "fillColor";
+
+		context.fillStyle = "fillColor";
 		context.beginPath();
 		context.arc(self.x, self.y, self.radius, 0, 2 * Math.PI);
 		context.fill();
 	};
+
 
 	self.reset = function (boardDirection) {
 		self.x = boardWidth / 2;
@@ -122,6 +95,7 @@ function Puck(x, y) {
 				x: 0,
 				y: Math.random() * boardHeight
 			};
+
 		} else if (boardDirection === BoardDirection.Right) {
 			// Start the puck heading right
 			randomPoint = {
@@ -183,8 +157,6 @@ function Puck(x, y) {
 			self.vel.x = self.vel.x - (2 * dotProd * normal.x);
 			self.vel.y = self.vel.x - (2 * dotProd * normal.y);
 			self.speed += 0.03;
-
-			count = 3;
 		}
 	};
 }
@@ -241,19 +213,8 @@ function Paddle(x, upKeyCode, downKeyCode) {
 		}
 	};
 
-	self.drawPaddle1 = function (context) {
-		context.fillStyle = "blue";
-
-		context.fillRect(
-			self.x - self.halfWidth,
-			self.y - self.halfHeight,
-			self.halfWidth * 2,
-			self.halfHeight * 2
-		);
-	};
-
-	self.drawPaddle2 = function (context) {
-		context.fillStyle = "red";
+	self.draw = function (context) {
+		context.fillStyle = "white";
 
 		context.fillRect(
 			self.x - self.halfWidth,
@@ -294,7 +255,7 @@ function init () {
 	canvas.width = boardWidth;
 	canvas.height = boardHeight;
 
-	puck = new Puck(920, 480);
+	puck = new Puck(100, 100);
 	paddle1 = new Paddle(10, 87, 83);
 	paddle2 = new Paddle(boardWidth - 10, 38, 40);
 
@@ -306,12 +267,6 @@ function init () {
 
 		if (e.keyCode === 13 && gameIsOver()) {
 			resetGame();
-		} else if (e.keyCode === 243 && gameIsOver()) {
-			titleScene();
-		}
-
-		if (e.keyCode === 13 && titleScene()) {
-			mainGameScene();
 		}
 	});
 
@@ -335,14 +290,13 @@ function resetGame() {
 	paddle1.reset();
 	paddle2.reset();
 
-	puck.reset(BoardDirection.Left, BoardDirection.Right);
+	puck.reset(BoardDirection.Left);
 
 	score1 = 0;
 	score2 = 0;
 }
 
 function update (dt) {
-	counter += 1;
 	puck.update(dt);
 	paddle1.update(dt);
 	paddle2.update(dt);
@@ -354,7 +308,6 @@ function update (dt) {
 function drawScore(context, score, boardDirection) {
 	var score = String(score);
 	context.font = "80px Sans";
-	context.fillStyle = "white";
 	var width = context.measureText(score).width;
 	var centerOffset = 240;
 
@@ -365,56 +318,27 @@ function drawScore(context, score, boardDirection) {
 	}
 }
 
-function drawTitleText(context, text, y) {
+function drawCenteredText(context, text, y) {
 	context.font = "100px Sans";
-	context.fillStyle = "white";
 	var width = context.measureText(text).width;
 
 	context.fillText(text, (boardWidth / 2) - (width / 2), y);
 }
 
-function titleScene(dt) {
-		drawTitleText(context, "Air Hockey Game", (boardHeight / 2.5));
-		drawTitleText(context, "Press Enter to Start", (boardHeight / 2) + 120);
+function render (dt) {
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	puck.draw(context);
+	paddle1.draw(context);
+	paddle2.draw(context);
 
-		return mainGameScene;
-}
+	drawScore(context, score1, BoardDirection.Left);
+	drawScore(context, score2, BoardDirection.Right);
 
-function mainGameScene(dt) {
-	if (!titleScene()) {
-		context.clearRect(0, 0, canvas.width, canvas.height);
-		puck.drawPuck(context);
-		paddle1.drawPaddle1(context);
-		paddle2.drawPaddle2(context);
-
-		drawScore(context, score1, BoardDirection.Left);
-		drawScore(context, score2, BoardDirection.Right);
-	}
-}
-
-function gameOverScene(dt) {
 	if (gameIsOver()) {
-		drawGameOverText(context, "Game Over", (boardHeight / 2.5));
-		drawGameOverText(context, "Press Enter to Retry", (boardHeight / 2.5) + 120);
-		drawEscapeText(context, "Press Escape key to return to title", (boardHeight / 2.5) + 240);
+		drawCenteredText(context, "Game Over", (boardHeight / 2));
+		drawCenteredText(context, "Press Enter to Retry", (boardHeight / 2) + 120);
 	}
-}
-
-function drawGameOverText(context, text, y) {
-	context.font = "100px Sans";
-	context.fillStyle = "white";
-	var width = context.measureText(text).width;
-
-	context.fillText(text, (boardWidth / 2) - (width / 2), y);
-}
-
-function drawEscapeText(context, text, y) {
-	context.font = "100px Sans";
-	context.fillStyle = "green";
-	var width = context.measureText(text).width;
-
-	context.fillText(text, (boardWidth / 2) - (width / 2), y);
-}
+};
 
 function main () {
 	var now = performance.now();
@@ -425,10 +349,8 @@ function main () {
 		dt = maxFrameTime;
 	}
 
-	titleScene(dt);
-	mainGameScene(dt);
-	gameOverScene(dt);
 	update(dt);
+	render(dt);
 
 	lastTime = now;
 
